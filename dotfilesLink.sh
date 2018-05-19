018f5baa5fd05581dc80404eb46f0066361c8c95
Yoshinobu Yamada <cancer_acropolis@yahoo.co.jp>
2018-05-19 08:08:13 +0000

    [fix] tmux_conf

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,5 +3,5 @@ set -eu
 # set -x
 
 ln -nfs ~/dotfiles/.bash_profile  ~/.bash_profile
-ln -nfs ~/dotfiles/.tmux_conf ~/.tmux.conf
+ln -nfs ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -nfs ~/dotfiles/.gitconfig  ~/.gitconfig
