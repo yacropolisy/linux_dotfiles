b9339d09acd397a8d346fbd4947097ff7b0590e6
Yoshinobu Yamada <cancer_acropolis@yahoo.co.jp>
2018-05-19 08:02:56 +0000

    first commit

---
+++ .bash_profile
@@ -0,0 +1,5 @@
+alias ll='ls -lG'
+alias ls='ls -G'
+alias grep='grep --color'
+export PS1='\[\033[34m\]\W \[\033[0m\]$ '
+
---
+++ dotfilesLink.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -eu
+# set -x
+
+ln -nfs ~/dotfiles/.bash_profile  ~/.bash_profile
+ln -nfs ~/dotfiles/.tmux_conf ~/.tmux.conf
+ln -nfs ~/dotfiles/.gitconfig  ~/.gitconfig
