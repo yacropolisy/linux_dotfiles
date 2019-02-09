07ead570e5212c4734e4882a7eaa9cd41cf6332f
Yoshinobu Yamada <cancer_acropolis@yahoo.co.jp>
2019-02-09 14:12:25 +0000

    [add] bashrc

---
+++ .bashrc
@@ -0,0 +1,3 @@
+alias ll='ls -lG'
+alias ls='ls -G'
+alias grep='grep --color'
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 # set -x
-
+ln -nfs ~/linux_dotfiles/.bashrc ~/.bashrc
 ln -nfs ~/linux_dotfiles/.bash_profile  ~/.bash_profile
 ln -nfs ~/linux_dotfiles/.tmux.conf ~/.tmux.conf
 ln -nfs ~/linux_dotfiles/.gitconfig  ~/.gitconfig
